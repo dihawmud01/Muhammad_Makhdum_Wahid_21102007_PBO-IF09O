ee4ab4f0dbbe51935265897e1845824171028663
ASUS M413DA <iammakhdumwahid@gmail.com>
2023-05-03 15:42:17 +0700

    Tambahan

---
+++ com/makhdum/pbo/pertemuan5/Barang.java
@@ -0,0 +1,22 @@
+package com.makhdum.pbo.pertemuan5;
+
+public class Barang {
+    //Atribute
+    String nama;
+    int harga;
+
+    // Constructor
+    public Barang(){}
+
+    public Barang(String nama, int harga){
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    // Method
+    public void ShowInfo(){
+        System.out.println("Nama Barang: " + this.nama);
+        System.out.println("Harga Barang: " + this.harga);
+        System.out.println();
+    }
+}
---
+++ com/makhdum/pbo/pertemuan5/Main.java
@@ -0,0 +1,40 @@
+package com.makhdum.pbo.pertemuan5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Barang[] keranjang = new Barang[10];
+
+        Barang[] etalase = {
+                new Barang("Indomie", 2000),
+                new Barang("Mie Sedaap", 2000),
+                new Barang("Beras", 12000),
+                new Barang("Gula", 15000),
+        };
+
+    // Melihat Panjang Array
+    System.out.println(
+            "Banyak barang di Etalase : "
+            +etalase.length);
+
+    etalase[0].ShowInfo();
+
+        System.out.println("Daftar barang di etalase: ");
+
+//        int i=1;
+//        for (Barang barang : etalase){
+//            System.out.println("Barang ke - " + i);
+//            barang.ShowInfo();
+//            i++;
+//        }
+        for (int j=0; j<etalase.length; j++) {
+            System.out.println("Barang ke - " + (j+1));
+            etalase[j].ShowInfo();
+        }
+
+        //Memasukkan barang ke keranjang
+        keranjang[0] = etalase[0];
+        System.out.println("Barang di keranjang : ");
+        keranjang[0].ShowInfo();
+        }
+    }
